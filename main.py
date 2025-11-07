78fea1c72f5d7b458858e87c1490835928fb0aaf
mitya298 <mitya298@gmail.com>
2025-11-07 17:36:53 +0300

    init

---
+++ main.py
@@ -0,0 +1,8 @@
+# This is a sample Python script.
+from lessons import lesson_1
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    lesson_1.operator_print()
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
